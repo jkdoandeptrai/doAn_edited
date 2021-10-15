81703408af0f59bb83fe1e9ad9b41dcb03e947d4
jkdoandeptrai <maidoan2002ns@gmail.com>
2021-10-15 10:28:52 +0700

    xong lan 1

--- Interface.sql
+++ Interface.sql
@@ -14,7 +14,7 @@ SELECT * FROM KH_DiemTichLuy( 3 /*Mốc điểm*/)
 EXEC Sinh_Nhat
 --Tìm mặt hàng có tên(gần giống) có đủ số lượng ít nhất mà khách hàng cần hay không
 
-SELECT * FROM Check_If_Available ('%',2)  ---(/*Tên hàng - Dùng được cả regular expression hay Wild card*/, /*Số lượng ít nhất cần tìm*/)
+EXEC Check_If_Available '%',2  ---(/*Tên hàng - Dùng được cả regular expression hay Wild card*/, /*Số lượng ít nhất cần tìm*/)
 
 -- Thêm khách hàng
                 INSERT INTO KhachHang(HO_VA_TEN,SDT,DIA_CHI,NGAY_SINH)
@@ -24,55 +24,60 @@ SELECT * FROM Check_If_Available ('%',2)  ---(/*Tên hàng - Dùng được cả
                             N'Thanh Hóa',-- DIA_CHI,
                             N'2001-02-10'-- NGAY_SINH
                         )
+        SELECT * FROM KhachHang
 
 -- Thêm hóa đơn cho khách hàng có mã là C_ID
 
  EXEC   ADD_BILL 
       --@param:
-        @C_ID ,
-        @NGUOI_NHAN , 
-        @DCGH ,
-        @STD, 
+        @C_ID = 10 ,
+        @NGUOI_NHAN = N'Mai Ngọc Đoàn' , 
+        @DCGH = N'Hoàng Mai' ,
+        @STD = '0385779443', 
         @PTTT , 
         @GC
 
+SELECT * FROM HoaDon
+SELECT * FROM MatHang_HD
+SELECT * FROM MatHang
+
 
 -- Cập nhật mặt hàng có trong hóa đơn có B_ID
-
  EXEC Cap_nhat_mat_hang_trong_hoa_don
       --@param:
-        @B_ID ,
-        @H_ID,
-        @amount,
-        @command,                                        -- command : 'end','add' = default,'delete'
-        @phiship
+        @B_ID = 110 ,
+        -- @H_ID = 102,
+        -- @amount = 3,
+        @command = 'end',                                        -- command : 'end','add' = default,'delete'
+        @phiship = 100000
 
-
-
-
--- Cập nhật tiền đơn hàng
-    
-
----------------------------------------------------------------------------------------------------------------------------------------------------------------
-
---                                                                     Phần Khách hàng                                                                       --
-
--- Khách hàng nhận được hàng, chỉ được xác nhận và đánh giá đơn hàng 1 lần
-        EXEC Customer_Received 100,N'Tốt',3 @WhichBill , @TinhTrang, @danhgia  
--- Khách hàng hủy đơn hàng có mã B_ID, điều kiện là đơn hàng đó phải chưa được shipper nào xác nhận cả.
-        EXEC Customer_Cancel 100  --@WhichBill
-
-
+SELECT * FROM MatHang_HD
 
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 --                                                                     Phần Shipper                                                                       --
      
-        EXEC Shipper_Confirm_Bill 1000,100 @praS,@praB  -- Shipper nào xác nhận đơn hàng có mã nào
+        EXEC Shipper_Confirm_Bill
+        @Who = 1000,
+        @WhichBill = 110 -- Shipper nào xác nhận đơn hàng có mã nào
 
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--                                                                     Phần Khách hàng                                                                       --
+
+-- Khách hàng nhận được hàng, chỉ được xác nhận và đánh giá đơn hàng 1 lần
+        EXEC Customer_Received 110,N'Tốt',3 @WhichBill , @TinhTrang, @danhgia  
+-- Khách hàng hủy đơn hàng có mã B_ID, điều kiện là đơn hàng đó phải chưa được shipper nào xác nhận cả.
+        EXEC Customer_Cancel 100  --@WhichBill
+
+
+
+
+
 
 --                                                                      Phần Ngoài                                                                       --
 
